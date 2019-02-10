126e3d9ebfc3d22f660ce43a34f2780c768b7c5e
Daniel McQuiston <daniel.mcq@gmail.com>
2019-02-10 13:49:34 -0700

    Adds some demo assets

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('Put real code here.')
+const assets = [];
+
+assets.push("Company 401k");
+assets.push("Credit Union Checking Account");
+assets.push("Stock market account");
+assets.push("Home mortgage");
+assets.push("Auto loan");
+
+console.log("My assets are: ", assets);
